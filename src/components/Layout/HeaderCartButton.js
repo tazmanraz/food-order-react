d146f6a84239ffdb83778794ad5cc07d9cb9c9b0
Imtiaz <imtiazmiah@gmail.com>
2021-10-10 01:14:28 -0400

    Add useContext hook for cart and implement proper cart item numbers

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,16 @@
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 import React from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0);
+
   return (
     <>
     <button className={classes.button} onClick={props.onClick}>
@@ -10,7 +18,7 @@ const HeaderCartButton = (props) => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>4</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
       
     </>
