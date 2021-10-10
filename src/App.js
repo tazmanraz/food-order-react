17d87a655c091638743d00828725f1ea8bc61597
Imtiaz <imtiazmiah@gmail.com>
2021-10-10 00:56:48 -0400

    Add cart state functionality of close and open

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartShown(false)
+  }
+  
   return (
     <>
-      <Cart />
-      <Header />
-      <main><Meals /></main>
+      {cartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
